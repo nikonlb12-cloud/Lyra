2252e8bae80d84fdec489f5e14378594c68be86a
nikonlb12-cloud <nikonlb12-cloud@users.noreply.github.com>
2026-03-30 03:51:24 +0100

    style: make dust particles bigger

--- lyra-site/src/shared.js
+++ lyra-site/src/shared.js
@@ -188,7 +188,7 @@ export function initParticles() {
       y: Math.random() * canvas.height,
       vx: (Math.random() - 0.5) * 0.3,
       vy: (Math.random() - 0.5) * 0.3,
-      size: Math.random() * 1.5 + 0.5,
+      size: Math.random() * 3.5 + 1.5,
       opacity: Math.random() * 0.5 + 0.1,
     };
   }
